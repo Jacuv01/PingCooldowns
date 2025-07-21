32079d552ea534fad949885c8dfd5d9fe21b6c0c
Jacuv <ID+username@users.noreply.github.com>
2025-07-21 01:55:10 -0600

    Revert "Refactor cooldown addons list to shared constant"
    This reverts commit f6bd2c2af7fdaa744ff71526d996b64a37d83f4b.

--- Modules/core/initialization.lua
+++ Modules/core/initialization.lua
@@ -1,17 +1,5 @@
 local _, PingCooldowns = ...
 
-local COOLDOWN_ADDONS = {
-    "EssentialCooldowns",
-    "UtilityCooldowns", 
-    "BuffIconCooldowns",
-    "WeakAuras",
-    "TellMeWhen",
-    "OmniCC",
-    "ElvUI",
-    "Bartender4",
-    "Dominos"
-}
-
 function PingCooldowns:OnLogin()
     self:Log("PingCooldowns addon loaded successfully!")
     
@@ -44,7 +32,19 @@ function PingCooldowns:PLAYER_ENTERING_WORLD(isInitialLogin, isReloadingUi)
 end
 
 function PingCooldowns:ADDON_LOADED(addonName)
-    for _, cooldownAddon in ipairs(COOLDOWN_ADDONS) do
+    local cooldownAddons = {
+        "EssentialCooldowns",
+        "UtilityCooldowns", 
+        "BuffIconCooldowns",
+        "WeakAuras",
+        "TellMeWhen",
+        "OmniCC",
+        "ElvUI",
+        "Bartender4",
+        "Dominos"
+    }
+    
+    for _, cooldownAddon in ipairs(cooldownAddons) do
         if addonName == cooldownAddon then
             self:Log("Cooldown addon detected: " .. addonName)
             C_Timer.After(0.5, function()
