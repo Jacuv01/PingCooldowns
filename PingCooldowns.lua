afc451928790825b5dc7110ee767887108ce0e64
Jacuv <ID+username@users.noreply.github.com>
2025-07-21 01:55:23 -0600

    Revert "Add talent and specialization event handling"
    This reverts commit d43ae23fff19ceb66bbe12e280065e4a335e0d25.

--- PingCooldowns.lua
+++ PingCooldowns.lua
@@ -38,26 +38,6 @@ PingCooldowns.frame:SetScript("OnEvent", function(self, event, ...)
         if PingCooldowns.SPELLS_CHANGED then
             PingCooldowns:SPELLS_CHANGED(...)
         end
-    elseif event == "PLAYER_TALENT_UPDATE" then
-        if PingCooldowns.PLAYER_TALENT_UPDATE then
-            PingCooldowns:PLAYER_TALENT_UPDATE(...)
-        end
-    elseif event == "ACTIVE_TALENT_GROUP_CHANGED" then
-        if PingCooldowns.ACTIVE_TALENT_GROUP_CHANGED then
-            PingCooldowns:ACTIVE_TALENT_GROUP_CHANGED(...)
-        end
-    elseif event == "TRAIT_CONFIG_UPDATED" then
-        if PingCooldowns.TRAIT_CONFIG_UPDATED then
-            PingCooldowns:TRAIT_CONFIG_UPDATED(...)
-        end
-    elseif event == "PLAYER_SPECIALIZATION_CHANGED" then
-        if PingCooldowns.PLAYER_SPECIALIZATION_CHANGED then
-            PingCooldowns:PLAYER_SPECIALIZATION_CHANGED(...)
-        end
-    elseif event == "UI_INFO_MESSAGE" then
-        if PingCooldowns.UI_INFO_MESSAGE then
-            PingCooldowns:UI_INFO_MESSAGE(...)
-        end
     end
 end)
 
